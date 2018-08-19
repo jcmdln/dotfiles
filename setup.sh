a9721ede287379e0227674b285ed082766968d6a
jcmdln <jcmdln@openbsd.my.domain>
2018-08-18 22:16:52 -0400

    Add simple setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env sh
+
+## Local user installation
+
+mkdir -p \
+      ~/.config/i3 \
+      ~/.bin
+
+ln -sf $(pwd)/etc/emacs/default.el ~/.emacs.el
+ln -sf $(pwd)/etc/vim/vimrc ~/.vimrc
+ln -sf $(pwd)/etc/tmux.conf ~/.tmux.conf
+ln -sf $(pwd)/etc/xinitrc ~/.xinitrc
+ln -sf $(pwd)/etc/i3/config ~/.config/i3/config
+ln -sf $(pwd)/etc/i3status.conf ~/.i3status.conf
+ln -sf $(pwd)/usr/local/bin/srandr ~/.bin/srandr
