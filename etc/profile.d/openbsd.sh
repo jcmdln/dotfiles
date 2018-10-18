e57b6286f055ffaa95f9e471a5fa40cddd71a916
jcmdln <jcmdln@gmail.com>
2018-10-18 11:00:26 -0400

    Don't define PKG_PATH, rely on /etc/installurl

--- etc/profile.d/openbsd.sh
+++ etc/profile.d/openbsd.sh
@@ -6,9 +6,4 @@ if [ "$(uname)" == "OpenBSD" ]; then
 	export PATH="$PATH:/usr/X11R6/bin"
 	export MANPATH="$MANPATH:/usr/X11R6/man"
     fi
-
-    ## Set package sources URL
-    PKG_PATH="https://mirror.leaseweb.net/pub/OpenBSD"
-    PKG_PATH="$PKG_PATH/$(uname -r)/packages/$(uname -m)"
-    export PKG_PATH
 fi
