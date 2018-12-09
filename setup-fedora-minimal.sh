74bb70fb47da6e7d37291ec0fbc1fddf2a4f0462
jcmdln <jcmdln@gmail.com>
2018-12-09 10:35:15 -0500

    cleanup pass; finish setup; etc

--- setup-fedora-minimal.sh
+++ setup-fedora-minimal.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
-
-packages_base="bind-utils connman dnf-utils git htop linux-firmware mlocate mosh pciutils pipewire tmux traceroute unzip usbutils vim whois"
-packages_desktop="arandr brightnessctl dmenu gnome-terminal i3 i3lock i3status mpv pulseaudio-utils pavucontrol scrot xinit xrandr xset"
-packages_fonts="google-noto-emoji-fonts google-noto-mono-fonts google-noto-sans-fonts google-noto-serif-fonts"
-packages_extras="brave-browser chromium chromium-libs-media-freeworld emacs-nox google-noto-cjk-fonts steam wireguard"
-
-services="connman"
+#
+# This script is intended to be used after performing a fedora network
+# install and choosing the base packages only. After the install has
+# completed and you have rebooted into the new system, download and run
+# this script.
+#
+##
 
 
 sudo dnf upgrade -y \
@@ -18,8 +18,9 @@ sudo dnf install -y \
     || exit
 
 sudo dnf install -y \
-     $packages_base $packages_desktop $packages_extras $packages_fonts \
+     arandr bind-utils brave-browser breeze breeze-cursor-theme breeze-gtk breeze-icon-theme brightnessctl chromium chromium-libs-media-freeworld connman dmenu dnf-utils emacs-nox git gnome-terminal gnome-terminal-nautilus google-noto-cjk-fonts google-noto-emoji-fonts google-noto-mono-fonts google-noto-sans-fonts google-noto-serif-fonts grub2-breeze-theme htop i3 i3lock i3status linux-firmware lxappearance mlocate mosh mpv nautilus pavucontrol pciutils pipewire plasma-breeze pulseaudio-utils scrot steam tmux traceroute tree unzip usbutils vim whois wireguard xinit xrandr xset \
     || exit
 
-sudo systemctl enable --now $services \
+sudo systemctl enable --now \
+     connman \
     || exit
--- setup.sh
+++ setup.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env sh
 
-## Local user installation
+dirs="~/.config"
+for dir in $dirs; do
+    if [ ! -d "$dir" ]; then
+	mkdir ~/.config
+    fi
+done
 
-mkdir -p \
-      ~/.config/i3 \
-      ~/.bin
+ln -sf $(pwd)/etc/bash/bashrc                     ~/.bashrc
+ln -sf $(pwd)/etc/emacs                           ~/.emacs.d
+ln -sf $(pwd)/etc/i3                              ~/.config/i3
+ln -sf $(pwd)/etc/i3status.conf                   ~/.i3status.conf
+ln -sf $(pwd)/etc/profile                         ~/.profile
+ln -sf $(pwd)/etc/profile.d                       ~/.profile.d
+ln -sf $(pwd)/etc/tmux.conf                       ~/.tmux.conf
+ln -sf $(pwd)/etc/vim/vimrc                       ~/.vimrc
+ln -sf $(pwd)/etc/xinitrc                         ~/.xinitrc
+ln -sf $(pwd)/usr/local/bin/srandr                ~/.local/bin/srandr
+ln -sf $(pwd)/usr/share/themes/gtk-2.0/gtkrc-2.0  ~/.gtkrc-2.0
+ln -sf $(pwd)/usr/share/themes/gtk-3.0            ~/.config/gtk-3.0
 
-ln -sf $(pwd)/etc/emacs/default.el ~/.emacs.el
-ln -sf $(pwd)/etc/vim/vimrc ~/.vimrc
-ln -sf $(pwd)/etc/tmux.conf ~/.tmux.conf
-ln -sf $(pwd)/etc/xinitrc ~/.xinitrc
-ln -sf $(pwd)/etc/i3/config ~/.config/i3/config
-ln -sf $(pwd)/etc/i3status.conf ~/.i3status.conf
-ln -sf $(pwd)/usr/local/bin/srandr ~/.bin/srandr
+sudo echo "GRUB_THEME=\"/boot/grub2/themes/breeze/theme.txt\"" >> /etc/default/grub
+sudo grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
