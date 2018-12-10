4e19d002dd3a0b2af33327e5f3291234bcbd569b
jcmdln <jcmdln@gmail.com>
2018-12-10 10:04:21 -0500

    Cleanup pass

--- setup.sh
+++ setup.sh
@@ -1,48 +1,36 @@
 #!/usr/bin/env sh
 
-dirs="/home/$USER/.config"
-for dir in $dirs; do
-    if [ ! -d $dir ]; then
-        mkdir $dir
-    fi
+dirs_create="
+    /home/$USER/.config
+    /home/$USER/.local/bin
+"
+for dir in $dirs_create; do
+    [ ! -d $dir ] && rm -rf $dir
+    [ ! -e $dir ] && mkdir $dir
+done
+
+dirs_remove="
+    /home/$USER/.emacs.d
+    /home/$USER/.config/i3
+    /home/$USER/.profile.d
+    /home/$USER/.vim
+"
+for dir in $dirs_remove; do
+    [ -e $dir ] && rm -rf $dir
 done
 
 ln -sf $(pwd)/etc/bash/bashrc                     ~/.bashrc
 ln -sf $(pwd)/etc/emacs                           ~/.emacs.d
-
-if [ -e /home/$USER/.config/i3 ]; then
-    rm -rf /home/$USER/.config/i3
-fi
-
 ln -sf $(pwd)/etc/i3                              ~/.config/i3
 ln -sf $(pwd)/etc/i3status.conf                   ~/.i3status.conf
-
-if [ -e /home/$USER/.profile.d ]; then
-    rm -rf /home/$USER/.profile.d
-fi
-
 ln -sf $(pwd)/etc/profile                         ~/.profile
 ln -sf $(pwd)/etc/profile.d                       ~/.profile.d
-
 ln -sf $(pwd)/etc/tmux.conf                       ~/.tmux.conf
-
-if [ -e /home/$USER/.vim ]; then
-    rm -rf /home/$USER/.vim
-fi
-
 ln -sf $(pwd)/etc/vim                             ~/.vim
-
 ln -sf $(pwd)/etc/xinitrc                         ~/.xinitrc
-
-if [ ! -e /home/$USER/.local/bin ]; then
-    mkdir -p /home/$USER/.local/bin
-fi
-
 ln -sf $(pwd)/usr/local/bin/srandr                ~/.local/bin/srandr
-
-
-#ln -sf $(pwd)/usr/share/themes/gtk-2.0/gtkrc-2.0  ~/.gtkrc-2.0
-#ln -sf $(pwd)/usr/share/themes/gtk-3.0            ~/.config/gtk-3.0
+ln -sf $(pwd)/usr/share/themes/gtk-2.0/gtkrc-2.0  ~/.gtkrc-2.0
+ln -sf $(pwd)/usr/share/themes/gtk-3.0            ~/.config/gtk-3.0
 
 #sudo echo "GRUB_THEME=\"/boot/grub2/themes/breeze/theme.txt\"" >> /etc/default/grub
 #sudo grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
