7bd1f34c650a57d74d36bfd9fcf6bccde9e6d18d
jcmdln <jcmdln@gmail.com>
2018-12-10 12:00:44 -0500

    Load existing built/compiled config
    This commit makes rebuilding and recompiling an optional step.
    Compilation should improve (emacs-init-time) and will be run
    automatically if no "config.el" is present. Should you want to force
    the configuration to be rebuilt and recompiled, run
    (emacs-build-config) manually when desired.

--- etc/emacs/init.el
+++ etc/emacs/init.el
@@ -1,6 +1,13 @@
 ;;; init.el
 
-(require 'org)
-(org-babel-load-file
- (expand-file-name "config.org"
-                   user-emacs-directory))
+(defun emacs-build-config()
+  "Rebuild and compile configuration."
+  (interactive)
+  (require 'org)
+  (org-babel-load-file
+   (expand-file-name "config.org" user-emacs-directory)
+   t))
+
+(if (file-exists-p "config.el")
+    (load-file "config.el")
+  (emacs-build-config))
