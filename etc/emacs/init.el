59bcf0e49824e2616551d102559bd46b3555fabe
jcmdln <jcmdln@gmail.com>
2018-12-20 18:26:15 -0500

    Automatically byte-compile

--- etc/emacs/init.el
+++ etc/emacs/init.el
@@ -1,5 +1,37 @@
 ;;; init.el
 
+(require 'package)
+(setq package-user-dir "~/.emacs.d/pkg/"
+      package-archives
+      '(("GNU ELPA"     . "http://elpa.gnu.org/packages/")
+        ("MELPA Stable" . "https://stable.melpa.org/packages/")
+        ("MELPA"        . "https://melpa.org/packages/"))
+      package-archive-priorities
+      '(("GNU ELPA"     . 1)
+        ("MELPA Stable" . 2)
+        ("MELPA"        . 0)))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package)
+  (require 'bind-key))
+
+(setq use-package-always-defer      t
+      use-package-always-ensure     t
+      use-package-check-before-init t)
+
+
 (require 'org)
 (org-babel-load-file
- (expand-file-name "config.org" user-emacs-directory))
+ (expand-file-name
+  "config.org" user-emacs-directory))
+
+(if (file-exists-p (expand-file-name
+		    "config.elc" user-emacs-directory))
+    ()
+  (byte-compile-file (expand-file-name
+		    "config.el" user-emacs-directory)))
