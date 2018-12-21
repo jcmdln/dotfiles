a5ace0641347cc9d67811ab30de9d75b9e862bf4
jcmdln <jcmdln@gmail.com>
2018-12-21 16:26:40 -0500

    Automatically re-compile config if out of date

--- etc/emacs/init.el
+++ etc/emacs/init.el
@@ -1,36 +1,13 @@
 ;;; init.el
 
-(require 'package)
-(setq package-user-dir "~/.emacs.d/pkg/"
-      package-archives
-      '(("GNU ELPA"     . "http://elpa.gnu.org/packages/")
-        ("MELPA Stable" . "https://stable.melpa.org/packages/")
-        ("MELPA"        . "https://melpa.org/packages/"))
-      package-archive-priorities
-      '(("GNU ELPA"     . 1)
-        ("MELPA Stable" . 2)
-        ("MELPA"        . 0)))
-(package-initialize)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(eval-when-compile
-  (require 'use-package)
-  (require 'bind-key))
-
-(setq use-package-always-defer      t
-      use-package-always-ensure     t
-      use-package-check-before-init t)
-
-(if (file-exists-p
-     (expand-file-name "config.elc" user-emacs-directory))
-    (load
-     (expand-file-name "config.elc" user-emacs-directory))
+(defun my-org-config()
+  (interactive)
   (require 'org)
-  (org-babel-load-file
-   (expand-file-name "config.org" user-emacs-directory))
-  (byte-compile-file
-   (expand-file-name "config.el" user-emacs-directory)))
+  (org-babel-load-file "~/.emacs.d/config.org")
+  (byte-compile-file "~/.emacs.d/config.el"))
 
+(if (file-exists-p "~/.emacs.d/config.elc")
+    (if (file-newer-than-file-p "~/.emacs.d/config.org" "~/.emacs.d/config.el")
+	(my-org-config)
+      (load "~/.emacs.d/config.el"))
+  (my-org-config))
