cd1ec9cfe1b88bfd27ccf04c1bf99f206eb8d8a2
jcmdln <jcmdln@gmail.com>
2018-12-30 21:56:10 -0500

    Add GC tweaks; cleanup pass

--- etc/emacs/init.el
+++ etc/emacs/init.el
@@ -1,13 +1,15 @@
 ;;; init.el
 
+;; Ensure config has been compiled and is not stale, otherwise rebuild
+;; and compile the configuration.
+
 (defun my-org-config()
+  "Build and compile 'org-mode' config."
   (interactive)
   (require 'org)
   (org-babel-load-file "~/.emacs.d/config.org")
   (byte-compile-file "~/.emacs.d/config.el"))
 
-(if (file-exists-p "~/.emacs.d/config.elc")
-    (if (file-newer-than-file-p "~/.emacs.d/config.org" "~/.emacs.d/config.el")
-	(my-org-config)
-      (load "~/.emacs.d/config.el"))
-  (my-org-config))
+(if (file-newer-than-file-p "~/.emacs.d/config.org" "~/.emacs.d/config.el")
+    (my-org-config)
+  (load "~/.emacs.d/config.el"))
