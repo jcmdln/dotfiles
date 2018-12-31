ca6e37d7b900128fab3e9f4565e07b3e26e0bd57
jcmdln <jcmdln@gmail.com>
2018-12-30 21:57:50 -0500

    Remove brave; safer grub theme config

--- setup-fedora.sh
+++ setup-fedora.sh
@@ -7,15 +7,6 @@
 #
 ##
 
-## Switch to Fedora Rawhide
-# sudo dnf install -y \
-#      fedora-release-rawhide dnf-utils \
-#      https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-rawhide.noarch.rpm
-# sudo dnf config-manager --set-disabled fedora updates updates-testing
-# sudo dnf config-managed --set-enabled rawhide
-# sudo dnf update dnf
-# sudo dnf -y --releasever=rawhide --setopt=deltarpm=false distro-sync --nogpgcheck
-
 sudo dnf upgrade -y \
     || exit
 
@@ -25,12 +16,14 @@ sudo dnf install -y \
     || exit
 
 sudo dnf install -y \
-     arandr bind-utils bpftrace brave-browser breeze-cursor-theme breeze-gtk breeze-icon-theme brightnessctl casync chromium chromium-libs-media-freeworld connman dmenu dnf-utils docker emacs-nox git gnome-terminal gnome-terminal-nautilus google-noto-cjk-fonts google-noto-emoji-fonts google-noto-mono-fonts google-noto-sans-fonts google-noto-serif-fonts grub2-breeze-theme htop i3 i3lock i3status linux-firmware lxappearance mlocate mosh mpv nautilus pavucontrol pciutils pipewire plasma-breeze pulseaudio-utils scrot steam tmux traceroute tree unzip usbutils vim whois wireguard xinit xrandr xset \
+     arandr bind-utils bpftrace breeze-cursor-theme breeze-gtk breeze-icon-theme brightnessctl casync chromium chromium-libs-media-freeworld cloc connman dmenu dnf-utils docker emacs-nox git gnome-terminal gnome-terminal-nautilus google-noto-cjk-fonts google-noto-emoji-fonts google-noto-mono-fonts google-noto-sans-fonts google-noto-serif-fonts grub2-breeze-theme htop i3 i3lock i3status linux-firmware lxappearance mlocate mosh mpv nautilus pavucontrol pciutils pipewire plasma-breeze pulseaudio-utils scrot steam tmux traceroute tree unzip usbutils vim whois wireguard xinit xrandr xset \
     || exit
 
 sudo systemctl enable --now \
      connman \
     || exit
 
-sudo echo "GRUB_THEME=/boot/grub2/themes/breeze/theme.txt" >> /etc/default/grub
-sudo grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
+if [ -e "/etc/default/grub" ] && [ -e "/boot/grub2/themes/breeze/theme.txt" ]; then
+    sudo echo "GRUB_THEME=/boot/grub2/themes/breeze/theme.txt" >> /etc/default/grub
+    sudo grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
+fi
