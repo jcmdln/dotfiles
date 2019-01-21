2b0778cf2f3d793875a57373644a39bc367720ac
jcmdln <jcmdln@gmail.com>
2019-01-21 13:12:05 -0500

    emacs: Improve formatting

--- etc/emacs/init.el
+++ etc/emacs/init.el
@@ -1,8 +1,5 @@
 ;;; init.el
 
-;; Ensure config has been compiled and is not stale, otherwise rebuild
-;; and compile the configuration.
-
 (defun my-org-config()
   "Build and compile 'org-mode' config."
   (interactive)
@@ -10,6 +7,11 @@
   (org-babel-load-file "~/.emacs.d/config.org")
   (byte-compile-file "~/.emacs.d/config.el"))
 
-(if (file-newer-than-file-p "~/.emacs.d/config.org" "~/.emacs.d/config.el")
+
+;; Ensure config has been compiled and is not stale, otherwise rebuild
+;; and compile the configuration.
+
+(if (file-newer-than-file-p "~/.emacs.d/config.org"
+			    "~/.emacs.d/config.el")
     (my-org-config)
   (load "~/.emacs.d/config.el"))
