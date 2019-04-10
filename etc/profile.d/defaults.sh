75b3a1c75783c92b801eb3a02742751fb9fdb953
jcmdln <jcmdln@gmail.com>
2019-04-09 22:31:30 -0400

    tmux: use xterm-256color and ignore the warnings

--- etc/profile.d/defaults.sh
+++ etc/profile.d/defaults.sh
@@ -5,7 +5,3 @@
 export LANG="en_US.UTF-8"
 export PAGER="less"
 export PROMPT_COMMAND="history -a"
-
-if [ "$TERM" = "tmux-256color" ] && [ -z "$TMUX" ]; then
-    export TERM="xterm-256color"
-fi
---
+++ etc/profile.d/tmux.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+# if [ "$TERM" = "tmux-256color" ] && [ -z "$TMUX" ]; then
+#     export TERM="xterm-256color"
+# fi
