3989a05624c5d3276cd081bbd7b2c0a9dd12067d
jcmdln <jcmdln@gmail.com>
2019-04-09 22:13:46 -0400

    etc/profile.d/vim.sh: use vimx in place of vim if present

--- etc/profile.d/vim.sh
+++ etc/profile.d/vim.sh
@@ -1,5 +1,10 @@
-# /etc/profile.d/vim.sh
+#!/usr/bin/env sh
 
-if [ -x $(command -v vimx) ]; then
+# Many operating systems provide vi, vim, and vimx packages, the latter
+# of which is usually compiled with extras like clipboard support among
+# other things. If vimx is present, then let's try to use it in place
+# of vim.
+
+if [ -n "$(command -v vimx)" ]; then
     alias vim="vimx"
 fi
