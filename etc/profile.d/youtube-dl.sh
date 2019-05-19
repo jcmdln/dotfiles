15b40f5057a169c9adbd084521ec6e3417c199f0
Johnathan C Maudlin <13183098+jcmdln@users.noreply.github.com>
2019-05-19 05:22:56 -0400

    Update youtube-dl.sh

--- etc/profile.d/youtube-dl.sh
+++ etc/profile.d/youtube-dl.sh
@@ -1,6 +1,6 @@
 # /etc/profile.d/youtube-dl.sh
 
 if [ -x $(command -v youtube-dl) ]; then
-    alias music-dl="youtube-dl --extract-audio --audio-format mp3 --audio-quality 0"
+    alias music-dl="youtube-dl --extract-audio --add-metadata --audio-format mp3 --audio-quality 0 -o '%(title)s.%(ext)s'"
     alias video-dl="youtube-dl -f best"
 fi
