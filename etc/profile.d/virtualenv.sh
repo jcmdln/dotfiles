d04b37fff85a6d6efe1ac8614f2638dec32bb545
jcmdln <jcmdln@gmail.com>
2020-01-12 20:30:58 -0500

    Add ceph-deploy & ceph-ansible venv aliases if present

--- etc/profile.d/virtualenv.sh
+++ etc/profile.d/virtualenv.sh
@@ -12,6 +12,14 @@ if [ -d ~/.local/virtualenv ]; then
         alias virtualenv_ansible=". ~/.local/virtualenv/ansible/bin/activate"
     fi
 
+    if [ -d ~/.local/virtualenv/ceph-ansible ]; then
+        alias virtualenv_ceph_ansible=". ~/.local/virtualenv/ceph-ansible/bin/activate"
+    fi
+
+    if [ -d ~/.local/virtualenv/ceph-deploy ]; then
+        alias virtualenv_ceph_deploy=". ~/.local/virtualenv/ceph-deploy/bin/activate"
+    fi
+
     if [ -d ~/.local/virtualenv/redfishtool ]; then
         alias virtualenv_redfishtool=". ~/.local/virtualenv/redfishtool/bin/activate"
     fi
