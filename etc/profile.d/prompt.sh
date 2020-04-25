98e6f43e0245c80e9d4f08c204969d63e8294cd5
jcmdln <jcmdln@gmail.com>
2020-04-25 13:57:52 -0400

    Better handling of PS1 and terminal emulator titles

--- etc/profile.d/prompt.sh
+++ etc/profile.d/prompt.sh
@@ -5,11 +5,11 @@ export PROMPT_COMMAND="history -a"
 if [ -e "$ZSH_VERSION" ]; then
     PS1="[%n@%M %d]$ "
 else
-    # Set prompt
     PS1="[\u@\h \W]\$ "
 
-    # Set terminal title
-    PS1="\[\e];\u@\h: \w\a\]$PS1"
+    if [ -n "$DISPLAY" ] || [ -n "$SSH_TTY" ]; then
+        PS1="\[\e];\u@\h: \w\a\]$PS1"
+    fi
 fi
 
 export PS1
