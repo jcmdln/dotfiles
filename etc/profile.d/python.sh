b4a1ec7b4b0942591ce6b2c2d38dee48f7f43fe1
jcmdln <jcmdln@gmail.com>
2020-07-08 13:33:12 -0400

    python: use environment variable rather than alias

--- etc/profile.d/python.sh
+++ etc/profile.d/python.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env sh
 
-PIP_OPTS="--disable-pip-version-check"
-
-if [ -x "$(command -v pip)" ]; then
-    alias pip="pip $PIP_OPTS"
-fi
-
-if [ -x "$(command -v pip2)" ]; then
-    alias pip2="pip2 $PIP_OPTS"
-fi
-
-if [ -x "$(command -v pip3)" ]; then
-    alias pip3="pip3 $PIP_OPTS"
-fi
-
-unset PIP_OPTS
+export PIP_DISABLE_PIP_VERSION_CHECK=1
 
 if [ -e "$HOME/.poetry/bin" ]; then
     export PATH="$HOME/.poetry/bin:$PATH"
