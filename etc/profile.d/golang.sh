36fa4c9a06881ee72c742821e73f08f4e4b25b49
jcmdln <jcmdln@gmail.com>
2020-08-11 17:42:41 -0400

    Enable GO111MODULE by default

--- etc/profile.d/golang.sh
+++ etc/profile.d/golang.sh
@@ -4,5 +4,6 @@ if [ -x "$(command -v go)" ]; then
     export GOPATH="$HOME/.local/go"
     export GOBIN="$GOPATH/bin"
     export GODOC="$GOPATH/doc"
+    export GO111MODULE="on"
     export PATH="$PATH:$GOBIN"
 fi
