fd449a13803216c952511200ab0f1d0e49a981e0
jcmdln <jcmdln@gmail.com>
2020-10-06 00:08:31 -0400

    profile: term: accidentially handled the invariant

--- .profile.d/term.sh
+++ .profile.d/term.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-if [ -n "$DISPLAY" ] && [ "$(tty | grep -c 'p')" -gt 0 ]; then
+if [ -n "$DISPLAY" ] || [ "$(tty | grep -c 'p')" -gt 0 ]; then
     TERM="xterm-256color"
 else
     TERM="vt220"
